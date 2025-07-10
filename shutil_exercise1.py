265a08545c4608614d69640784a143921968dde3
He3amtesla <128234173+He3amtesla@users.noreply.github.com>
2025-07-10 16:00:43 +0330

    Delete shutil_exercise1.py

--- shutil_exercise1.py
+++
@@ -1,8 +0,0 @@
-import os
-import shutil
-
-os.makedirs("./test_dir", exist_ok=True)
-with open("./test_dir/hello.txt","w") as f:
-    f.write("salam")
-
-shutil.copy("./test_dir/hello.txt", "./test_dir/hello_copy.txt")
